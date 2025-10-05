eebad2a11e8d49894ada8970bab413b5fc488c46
Dermawan-Evan-Kaisar <mikymolly0@gmail.com>
2025-10-05 19:30:55 +0700

    Tugas mini calculator

---
+++ style.js
@@ -0,0 +1,22 @@
+const Tambah = (a ,b) => a + b;
+const Kurang = (a, b) => a - b;
+const Kali = (a, b) => a * b;
+const Bagi = (a, b) => a / b;
+const Modulus = (a, b) => a % b;
+
+let a = 10;
+let b = 5;
+
+console.log(`Ini nilai a = ${a}`);
+console.log(`Ini nilai b = ${b}`);
+
+
+
+console.log(`Hasil tambah a + b = ${Tambah(a,b)}`);
+console.log(`Hasil kurang a - b = ${Kurang(a,b)}`);
+console.log(`Hasil kali a * b = ${Kali(a,b)}`);
+console.log(`Hasil bagi a / b = ${Bagi(a,b)}`);
+console.log(`Hasil modulus a % b = ${Modulus(a,b)}`);
+
+
+
